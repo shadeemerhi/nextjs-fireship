6fed6232d274426eae97f4e361007f6ad545be63
shadeemerhi <shadmerhi@gmail.com>
2022-05-15 11:28:02 -0600

    add sign in with google

--- pages/enter.js
+++ pages/enter.js
@@ -1,13 +1,46 @@
 import Head from "next/head";
+import { auth } from "../lib/firebase";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
+import { Box, Button } from "@mantine/core";
 
 const EnterPage = () => {
+  const user = null;
+  const username = null;
   return (
     <main>
-      <Head>
-        <title>Enter</title>
-      </Head>
-      <h1>Enter Page</h1>
+      <Box p={80}>
+        {user ? (
+          username ? (
+            <SignOutButton />
+          ) : (
+            <UsernameForm />
+          )
+        ) : (
+          <SignInButton />
+        )}
+      </Box>
     </main>
+  );
+};
+
+const UsernameForm = () => {};
+
+const SignInButton = () => {
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+  return (
+    <Button color="white" variant="outline" onClick={() => signInWithGoogle()}>
+      Continue with Google
+    </Button>
+  );
+};
+
+const SignOutButton = () => {
+  return (
+    <Button variant="outline" onClick={() => signOut()}>
+      Sign Out
+    </Button>
   );
 };
 
